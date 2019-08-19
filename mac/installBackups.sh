0ba5520c3e70f02e72c8b5b1714656980dd60074
Terence <35042213+reshinto@users.noreply.github.com>
2019-08-19 23:23:32 +0800

    Update installBackups.sh

--- mac/installBackups.sh
+++ mac/installBackups.sh
@@ -135,3 +135,6 @@ sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 # install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
