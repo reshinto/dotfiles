8fe9086218bb2d69ca718031ddbc092cd8216870
Terence <35042213+reshinto@users.noreply.github.com>
2019-09-15 04:52:11 +0800

    Update installBackups.sh

--- mac/installBackups.sh
+++ mac/installBackups.sh
@@ -137,6 +137,14 @@ jsArr=(
 
 install "npm -g i" "${jsArr[@]}"
 
+# gem packages
+rbArr=(
+  "irb"
+  "rails"
+)
+
+install "gem install" "${rbArr[@]}"
+
 # install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
