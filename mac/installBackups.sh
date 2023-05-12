19fec02610a88e12e5539c6c9a679c7d6864ccd7
Terence <35042213+reshinto@users.noreply.github.com>
2023-05-12 22:01:33 +0800

    Update installBackups.sh

--- mac/installBackups.sh
+++ mac/installBackups.sh
@@ -17,7 +17,7 @@ function install() {
 xcode-select --install
 
 # install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # brew packages
 brewArr=(
