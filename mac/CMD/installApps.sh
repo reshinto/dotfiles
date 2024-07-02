57c25ff7aa28cb32a9359cddba40626c04678ddf
Terence <35042213+reshinto@users.noreply.github.com>
2024-07-02 17:43:36 +0800

    Create installApps.sh

---
+++ mac/CMD/installApps.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+# chmod a+x installApps.sh
+
+function install() {
+  # 1st arg, command value
+  cmd=$1
+  shift
+  # 2nd arg, array
+  arr=("$@")
+  for i in "${arr[@]}";
+  do
+    $cmd "$i"
+  done
+}
+
+echo export PATH=$PATH:/opt/homebrew/bin >> .zshrc
+source ~/.zshrc
+
+# brew packages
+brewArr=(
+  "macvim"
+)
+
+install "brew install" "${brewArr[@]}"
+
+# brew cask
+# https://caskroom.github.io/search
+brewCaskArr=(
+  "iterm2"
+  "vlc"
+  "google-chrome"
+  "google-hangouts"
+  "firefox"
+  "whatsapp"
+  "dropbox"
+  "appcleaner"
+  "grammarly"
+  "rectangle"
+  "unpkg"
+  "licecap"
+  "nordvpn"
+  "zoom"
+  "microsoft-teams"
+  "macfuse"
+  "telegram"
+  "microsoft-edge"
+  "voov-meeting"
+)
+
+install "brew install --cask" "${brewCaskArr[@]}"
